03c40f96d1271386b7b4a80d0d9fad6ef9e34dd5
root <root@ferkomedia.sk>
2026-01-08 10:43:26 +0100

    Deploy Astro frontend on VPS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'static',
+  preview: {
+    host: '0.0.0.0',
+    port: 4321,
+  },
+});
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+  server: {
+    middlewareMode: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4321,
+    allowedHosts: ['eshop.brxa.sk', 'localhost', '127.0.0.1'],
+  },
+};
